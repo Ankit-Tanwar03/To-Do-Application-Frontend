f1eb5c0468213e4e148c9f0d009153dfefe46241
Ankit Tanwar <ankittanwar.y08@gmail.com>
2024-02-20 14:42:47 +0100

    Create User added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Create_account from './Components/Create_account';
 import Login from './Components/Login';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Login />
+      <Routes>
+      <Route path = "/" element={<Navigate replace to="/login" />} />
+      <Route path = "/login" element={<Login />} />
+      <Route path = "/createAccount" element={<Create_account />}/>
+      </Routes>
     </div>
   );
 }
